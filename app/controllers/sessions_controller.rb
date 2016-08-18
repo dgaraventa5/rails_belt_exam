e89daf62fb5099b11c6b581a1493e519540dfac3
John Doe <johndoe@example.com>
2016-08-18 11:00:32 -0700

    login_reg

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
+  # login and reg page, have user for form helper
   def new
   end
 
+  # login
   def create
+  	@user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password]) == false
+      redirect_to "/"
+    else
+      session[:user_id] = @user.id
+      redirect_to "/events"
+    end
   end
 
+  # logout 
   def destroy
+  	reset_session
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
   get 'sessions/new'
 
   get 'sessions/create'
